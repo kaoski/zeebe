c46ed6a40b15630928f0c88d52e88d0a110aaf63
Nico korthout <nico.korthout@camunda.com>
2020-11-12 15:36:55 +0100

    chore(ci): split IT test-compile from verify

--- .ci/scripts/distribution/it-java.sh
+++ .ci/scripts/distribution/it-java.sh
@@ -5,7 +5,9 @@ export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -XX:MaxRAMFraction=$((LIMITS_CPU))"
 
 tmpfile=$(mktemp)
 
-mvn -B --fail-never -T$LIMITS_CPU -s ${MAVEN_SETTINGS_XML} verify -P skip-unstable-ci,parallel-tests -pl qa/integration-tests -pl upgrade-tests -DtestMavenId=2 -Dsurefire.rerunFailingTestsCount=7 | tee ${tmpfile}
+mvn -B -T$LIMITS_CPU -s ${MAVEN_SETTINGS_XML} org.apache.maven.plugins:maven-dependency-plugin:3.1.2:go-offline org.apache.maven.plugins:maven-dependency-plugin:3.1.2:resolve-plugins -Pskip-unstable-ci,prepare-offline
+
+mvn -o -B --fail-never -T$LIMITS_CPU -s ${MAVEN_SETTINGS_XML} verify -P skip-unstable-ci,parallel-tests -pl qa/integration-tests -pl upgrade-tests -DtestMavenId=2 -Dsurefire.rerunFailingTestsCount=7 | tee ${tmpfile}
 
 status=${PIPESTATUS[0]}
 
